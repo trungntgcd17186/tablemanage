25391f9cf4da30195c8d33cb3173f7ea6727d7f9
Trung <thanhtrung13111997@gmail.com>
2022-03-16 12:08:59 +0700

    add style

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -1,146 +1,9 @@
-import { Table, Row, Col, Select, Button, Input } from "antd";
+import { Table, Row, Col, Select, Button, Input, DatePicker } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
 import "antd/dist/antd.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
-const columns = [
-  {
-    title: "Quote ID",
-    dataIndex: "key",
-    key: "key",
-    render(text: any) {
-      return {
-        props: {
-          style: { color: "#008DFF" },
-        },
-        children: <span>{text}</span>,
-      };
-    },
-  },
-  {
-    title: "Care Recipient Name",
-    dataIndex: "care_recipient_name",
-    key: "care_recipient_name",
-  },
-  {
-    title: "Care Recipient DOB",
-    dataIndex: "care_recipient_dob",
-    key: "care_recipient_dob",
-  },
-  {
-    title: "Rate",
-    dataIndex: "rate",
-    key: "rate",
-  },
-  {
-    title: "Short Temp",
-    dataIndex: "short_temp",
-    key: "short_temp",
-    render(text: any) {
-      return {
-        props: {
-          style: { color: text === "YES" ? "#008DFF" : "red" },
-        },
-        children: <span>{text}</span>,
-      };
-    },
-  },
-  {
-    title: "Contagion",
-    dataIndex: "contagion",
-    key: "contagion",
-    render(text: any) {
-      return {
-        props: {
-          style: { color: text === "YES" ? "#008DFF" : "red" },
-        },
-        children: <span>{text}</span>,
-      };
-    },
-  },
-  {
-    title: "Contagion",
-    dataIndex: "contagion",
-    key: "contagion",
-    render(text: any) {
-      return {
-        props: {
-          style: { color: text === "YES" ? "#008DFF" : "red" },
-        },
-        children: <span>{text}</span>,
-      };
-    },
-  },
-  {
-    title: "Emergency",
-    dataIndex: "emergency",
-    key: "emergency",
-    render(text: any) {
-      return {
-        props: {
-          style: { color: text === "YES" ? "#008DFF" : "red" },
-        },
-        children: <span>{text}</span>,
-      };
-    },
-  },
-  {
-    title: "Mileage Surcharge",
-    dataIndex: "mileage_surcharge",
-    key: "mileage_surcharge",
-    render(text: any) {
-      return {
-        props: {
-          style: { color: text === "YES" ? "#008DFF" : "red" },
-        },
-        children: <span>{text}</span>,
-      };
-    },
-  },
-  {
-    title: "Primary Quote",
-    dataIndex: "primary_quote",
-    key: "primary_quote",
-    render(text: any) {
-      return {
-        props: {
-          style: { color: text === "YES" ? "#008DFF" : "red" },
-        },
-        children: <span>{text}</span>,
-      };
-    },
-  },
-  {
-    title: "Start Date",
-    dataIndex: "start_date",
-    key: "start_date",
-  },
-  {
-    title: "Created Date",
-    dataIndex: "created_date",
-    key: "created_date",
-  },
-  {
-    title: "Created_By",
-    dataIndex: "created_by",
-    key: "created_by",
-  },
-  {
-    title: "Updated_Date",
-    dataIndex: "updated_date",
-    key: "updated_date",
-  },
-  {
-    title: "Status",
-    dataIndex: "status",
-    key: "status",
-  },
-  {
-    title: "...",
-    render: () => <DeleteOutlined />,
-  },
-];
+import "./index.css";
 
 const rowSelection = {
   onChange: (selectedRowKeys: React.Key[], selectedRows: any) => {
@@ -153,7 +16,7 @@ const rowSelection = {
 };
 
 export default function TableContent() {
-  const [datas, setDatas] = useState([]);
+  const [datas, setDatas] = useState<any[]>([]);
   const selectionType = "checkbox";
 
   useEffect(() => {
@@ -161,6 +24,375 @@ export default function TableContent() {
       setDatas(response.data);
     });
   }, []);
+
+  const columns = [
+    {
+      title: () => {
+        return (
+          <div style={{ textAlign: "center" }}>
+            Quote ID
+            <Input />
+          </div>
+        );
+      },
+      dataIndex: "key",
+      key: "key",
+      render(text: any) {
+        return {
+          props: {
+            style: { color: "#008DFF" },
+          },
+          children: <span>{text ? text.slice(0, 9) : ""}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              width: "150px",
+              textAlign: "center",
+            }}
+          >
+            Care Recipient Name
+            <Input />
+          </div>
+        );
+      },
+      dataIndex: "care_recipient_name",
+      key: "care_recipient_name",
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              width: "150px",
+              textAlign: "center",
+            }}
+          >
+            Care Recipient DOB
+            <DatePicker />
+          </div>
+        );
+      },
+      dataIndex: "care_recipient_dob",
+      key: "care_recipient_dob",
+      render(text: any) {
+        return {
+          props: {
+            style: { textAlign: "right" },
+          },
+          children: <span>{text}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              width: "80px",
+              textAlign: "center",
+              height: "54px",
+            }}
+          >
+            Hour Rate
+          </div>
+        );
+      },
+      dataIndex: "rate",
+      key: "rate",
+      render(text: any) {
+        return {
+          props: {
+            style: { textAlign: "right" },
+          },
+          children: <span>{text}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "80px",
+              textAlign: "center",
+            }}
+          >
+            Short Term
+            <Select onChange={handleChangeInput}>
+              <Option value="yes">YES</Option>
+              <Option value="no">NO</Option>
+            </Select>
+          </div>
+        );
+      },
+      dataIndex: "short_temp",
+      key: "short_temp",
+      render(text: any) {
+        return {
+          props: {
+            style: {
+              color: text === true ? "#008DFF" : "red",
+              textAlign: "center",
+            },
+          },
+          children: <span>{text === true ? "YES" : "NO"}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "80px",
+              textAlign: "center",
+            }}
+          >
+            Contagion
+            <Select onChange={handleChangeInput}>
+              <Option value="yes">YES</Option>
+              <Option value="no">NO</Option>
+            </Select>
+          </div>
+        );
+      },
+      dataIndex: "contagion",
+      key: "contagion",
+      render(text: any) {
+        return {
+          props: {
+            style: {
+              color: text === true ? "#008DFF" : "red",
+              textAlign: "center",
+            },
+          },
+          children: <span>{text === true ? "YES" : "NO"}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "80px",
+              textAlign: "center",
+            }}
+          >
+            Emergency
+            <Select onChange={handleChangeInput}>
+              <Option value="yes">YES</Option>
+              <Option value="no">NO</Option>
+            </Select>
+          </div>
+        );
+      },
+      dataIndex: "emergency",
+      key: "emergency",
+      render(text: any) {
+        return {
+          props: {
+            style: {
+              color: text === true ? "#008DFF" : "red",
+              textAlign: "center",
+            },
+          },
+          children: <span>{text === true ? "YES" : "NO"}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "130px",
+              textAlign: "center",
+            }}
+          >
+            Mileage Surcharge
+            <Select onChange={handleChangeInput}>
+              <Option value="yes">YES</Option>
+              <Option value="no">NO</Option>
+            </Select>
+          </div>
+        );
+      },
+      dataIndex: "mileage_surcharge",
+      key: "mileage_surcharge",
+      render(text: any) {
+        return {
+          props: {
+            style: {
+              color: text === true ? "#008DFF" : "red",
+              textAlign: "center",
+            },
+          },
+          children: <span>{text === true ? "YES" : "NO"}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "105px",
+              textAlign: "center",
+            }}
+          >
+            Primary Quote
+            <Select onChange={handleChangeInput}>
+              <Option value="yes">YES</Option>
+              <Option value="no">NO</Option>
+            </Select>
+          </div>
+        );
+      },
+      dataIndex: "primary_quote",
+      key: "primary_quote",
+      render(text: any) {
+        return {
+          props: {
+            style: {
+              color: text === true ? "#008DFF" : "red",
+              textAlign: "center",
+            },
+          },
+          children: <span>{text === true ? "YES" : "NO"}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              width: "150px",
+              textAlign: "center",
+            }}
+          >
+            Start Date
+            <DatePicker />
+          </div>
+        );
+      },
+      dataIndex: "start_date",
+      key: "start_date",
+      render(text: any) {
+        return {
+          props: {
+            style: { textAlign: "right" },
+          },
+          children: <span>{text}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return <div className="title-created">Created Date</div>;
+      },
+      dataIndex: "created_date",
+      key: "created_date",
+      sorter: (a: IDataType, b: IDataType) => {
+        return (
+          new Date(a.created_date).getTime() -
+          new Date(b.created_date).getTime()
+        );
+      },
+      render(text: any) {
+        return {
+          props: {
+            style: { textAlign: "right" },
+          },
+          children: <span>{text}</span>,
+        };
+      },
+    },
+    {
+      title: () => {
+        return <div className="title-created">Created By</div>;
+      },
+      dataIndex: "created_by",
+      key: "created_by",
+      sorter: (a: IDataType, b: IDataType) => {
+        return a.created_by.length - b.created_by.length;
+      },
+    },
+    {
+      title: () => {
+        return <div className="title-created">Updated Date</div>;
+      },
+      dataIndex: "updated_date",
+      key: "updated_date",
+      sorter: (a: IDataType, b: IDataType) => {
+        return (
+          new Date(a.updated_date).getTime() -
+          new Date(b.updated_date).getTime()
+        );
+      },
+    },
+    {
+      title: () => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "105px",
+              textAlign: "center",
+            }}
+          >
+            Status
+            <Select showSearch placeholder="Select a status">
+              <Option value="new">new</Option>
+              <Option value="approved">approved</Option>
+              <Option value="rejected">rejected</Option>
+              <Option value="closed">closed</Option>
+            </Select>
+          </div>
+        );
+      },
+      dataIndex: "status",
+      key: "status",
+      render(text: any) {
+        return {
+          props: {
+            style: { textAlign: "center" },
+          },
+          children: <div className="status">{text}</div>,
+        };
+      },
+    },
+    {
+      title: "...",
+      render: () => <DeleteOutlined />,
+    },
+  ];
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+
+    const newData: IDataType[] = datas.filter(
+      (data) => data.short_temp === true
+    );
+    setDatas(newData);
+  };
 
   const { Option } = Select;
   function handleChange(value: any) {
---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+interface IDataType {
+  key: React.Key;
+  quote_id: string;
+  name: string;
+  birthday: dateTime;
+  rate: number;
+  short_term: boolean;
+  contagion: boolean;
+  emergency: boolean;
+  mileage_surcharge: boolean;
+  primary_quote: boolean;
+  start_date: dateTime;
+  created_date: dateTime;
+  created_by: string;
+  updated_date: dateTime;
+  status: string;
+}
